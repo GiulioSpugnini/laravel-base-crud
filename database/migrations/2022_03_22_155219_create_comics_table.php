b2fec7347273583b11d6559bcae37350f1fd2db9
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-03-22 17:07:39 +0100

    added all tables at db

--- database/migrations/2022_03_22_155219_create_comics_table.php
+++ database/migrations/2022_03_22_155219_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('thumb');
+            $table->float('price', 6, 2);
+            $table->string('series');
+            $table->dateTime('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
