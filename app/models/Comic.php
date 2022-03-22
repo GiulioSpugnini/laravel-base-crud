f4a989c184a75d79b8c1afee5c0d643860434f50
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-03-22 17:22:36 +0100

    added seeder

--- app/models/Comic.php
+++ app/models/Comic.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\models\Comic;
+namespace App\Models\Comic;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Comic extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'description', 'thumb', 'price', 'series', 'sale_date', 'type'
+    ];
 }
