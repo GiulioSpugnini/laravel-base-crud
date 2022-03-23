922534786f22d8e52d6f03323b9875f5550522c8
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-03-23 15:32:59 +0100

    linked home route at layout navbar and added link in the home to comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.main')
 
 @section('content')
+    <div class="container">
+
+        <a href="{{ route('comics.index') }}">
+            <h2>
+                Fumetti
+            </h2>
+        </a>
+    </div>
 @endsection
